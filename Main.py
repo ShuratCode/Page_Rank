fca21e7d3db264553991122331f53d4fcf1d95ca
Shaked Eyal <shaked.eyal@gmail.com>
2019-03-26 17:54:44 +0200

    fix sort

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from graph import Graph
 
 if __name__ == '__main__':
     g = Graph()
-    g.load_graph(r"C:\Users\Shaked Eyal\IdeaProjects\Page_Rank\Resources\example.csv")
+    g.load_graph(r"C:\Users\Shaked Eyal\IdeaProjects\Page_Rank\Resources\wiki.csv")
     g.calculate_page_rank()
-    print(g.page_rank)
+    all_page_ranks = g.get_all_PageRank()
+    for key, value in all_page_ranks:
+        print(key, value)
--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 import os.path
 from collections import Counter
 from collections import OrderedDict
+from operator import itemgetter
 
 import pandas as pd
 
@@ -11,8 +12,6 @@ class Graph:
         self.nodes_source = {}  # I am the pointing to the nodes in the list
         self.nodes_dest = {}  # I am being pointed by the nodes in the list
         self.page_rank = {}
-        self.delta = 0.001
-        self.beta = 0.85
 
     def load_graph(self, path):
         if not os.path.isfile(path):
@@ -47,29 +46,48 @@ class Graph:
         for node in self.page_rank.keys():
             self.page_rank[node] = rank
 
-    def calculate_page_rank(self):
+    def calculate_page_rank(self, beta=0.85, delta=0.001):
         new_page_rank = {}
         for i in range(0, 20):
             for node in self.page_rank.keys():
                 sum_in_ranks = 0
-                for source in self.nodes_dest[node]:
-                    d_i = len(self.nodes_source[source])
-                    former_rank = self.page_rank[source]
-                    normalize_rank = self.beta * (former_rank / d_i)
-                    sum_in_ranks += normalize_rank
+                if node in self.nodes_dest.keys():
+                    for source in self.nodes_dest[node]:
+                        if source in self.nodes_source.keys():
+                            d_i = len(self.nodes_source[source])
+                            former_rank = self.page_rank[source]
+                            normalize_rank = beta * (former_rank / d_i)
+                            sum_in_ranks += normalize_rank
                 new_page_rank[node] = sum_in_ranks
+            new_page_rank = self.re_insert_leaked_page_rank(new_page_rank)
             old_page_rank = self.page_rank
             self.page_rank = new_page_rank
             new_page_rank = {}
-            if self.delta_check(old_page_rank):
+            if self.delta_check(old_page_rank, delta):
                 break
 
-    def delta_check(self, page_rank):
+    def re_insert_leaked_page_rank(self, page_rank):
+        copy_page_rank = page_rank.copy()
+        sum_of_page_ranks = self.sum_of_ranks(page_rank)
+        num_of_nodes = len(page_rank)
+        for node in page_rank.keys():
+            rank = page_rank[node]
+            addition = (1 - sum_of_page_ranks) / num_of_nodes
+            copy_page_rank[node] = rank + addition
+        return copy_page_rank
+
+    def sum_of_ranks(self, page_rank):
+        sum_of_ranks = 0
+        for rank in page_rank.values():
+            sum_of_ranks += rank
+        return sum_of_ranks
+
+    def delta_check(self, page_rank, delta):
         sum_delta = 0
         for node in self.page_rank.keys():
             new_delta = abs(page_rank[node] - self.page_rank[node])
             sum_delta += new_delta
-        return self.delta > sum_delta
+        return delta > sum_delta
 
     def get_PageRank(self, node_name):
         node = int(node_name)
@@ -82,5 +100,5 @@ class Graph:
         return top_n
 
     def get_all_PageRank(self):
-        sorted_dict = OrderedDict(self.page_rank)
+        sorted_dict = sorted(self.page_rank.items(), key=itemgetter(1), reverse=True)
         return sorted_dict
